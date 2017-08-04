f3ee3beadc8ba5abc6fe323355f04470cf874c16
Masato25 <leomasato25@gmail.com>
2017-08-04 10:09:58 +0800

    update user view for http response

--- web/views/user_view.ex
+++ web/views/user_view.ex
@@ -10,10 +10,18 @@ defmodule ParpServer.UserView do
   end
 
   def render("user.json", %{user: user}) do
-    %{id: user.id,
+    %{
+      id: user.id,
+      username: user.username,
       name: user.name,
       gender: user.gender,
       birthday: user.birthday,
-      parking_license: user.parking_license}
+      parking_license: user.parking_license,
+      payment: user.payment
+    }
+  end
+
+  def render("showwithsession.json", %{user: user, session: session}) do
+    %{data: render_one(user, ParpServer.UserView, "user.json"), session: %{username: user.username, session: session.session_key}}
   end
 end
